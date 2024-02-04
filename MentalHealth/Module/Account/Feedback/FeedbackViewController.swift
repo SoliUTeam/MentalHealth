f4a36572e55f3b659de00c4fa8b358df6fb6645c
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-02-04 09:47:58 -0600

    chore: file re-struct

---
+++ MentalHealth/Module/Account/Feedback/FeedbackViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FeedbackViewController.swift
+//  MentalHealth
+//
+//  Created by JungpyoHong on 2/4/24.
+//
+
+import Foundation
+import UIKit
+
+class FeedbackViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
