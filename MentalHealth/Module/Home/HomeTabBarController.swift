c60520b1468dfa7a6fa6ef35e5e5820999277469
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-03-21 23:28:07 -0500

    edit: tab bar controller

--- MentalHealth/Module/Home/HomeTabBarController.swift
+++ MentalHealth/Module/Home/HomeTabBarController.swift
@@ -44,55 +44,77 @@ extension HomeTabBarController: UITabBarControllerDelegate {
 }
 
 @IBDesignable class TabBarWithCorners: UITabBar {
-    @IBInspectable var color: UIColor?
-    @IBInspectable var radii: CGFloat = 18
-    
+
     private var shapeLayer: CALayer?
-    
-    override func draw(_ rect: CGRect) {
-        addShape()
-    }
-    
+
     private func addShape() {
         let shapeLayer = CAShapeLayer()
-        
         shapeLayer.path = createPath()
         shapeLayer.strokeColor = UIColor.lightGray.cgColor
-        shapeLayer.fillColor = color?.cgColor ?? UIColor.white.cgColor
-        shapeLayer.lineWidth = 1
-        shapeLayer.shadowColor = UIColor.lightGray.cgColor
-        shapeLayer.shadowOffset = CGSize(width: 0, height: -2);
-        shapeLayer.shadowOpacity = 0.21
-        shapeLayer.shadowRadius = 8
-        shapeLayer.shadowPath =  UIBezierPath(roundedRect: bounds, cornerRadius: radii).cgPath
-        
+        shapeLayer.fillColor = UIColor.white.cgColor
+        shapeLayer.lineWidth = 1.0
+
         if let oldShapeLayer = self.shapeLayer {
-            layer.replaceSublayer(oldShapeLayer, with: shapeLayer)
+            self.layer.replaceSublayer(oldShapeLayer, with: shapeLayer)
         } else {
-            layer.insertSublayer(shapeLayer, at: 0)
+            self.layer.insertSublayer(shapeLayer, at: 0)
         }
-        
+
         self.shapeLayer = shapeLayer
     }
-    
-    private func createPath() -> CGPath {
-        let path = UIBezierPath(
-            roundedRect: bounds,
-            byRoundingCorners: [.topLeft, .topRight],
-            cornerRadii: CGSize(width: radii, height: 0.0))
+
+    override func draw(_ rect: CGRect) {
+        self.addShape()
+    }
+
+    func createPath() -> CGPath {
+
+        let height: CGFloat = 37.0
+        let path = UIBezierPath()
+        let centerWidth = self.frame.width / 2
+
+        path.move(to: CGPoint(x: 0, y: 0)) // start top left
+        path.addLine(to: CGPoint(x: (centerWidth - height * 2), y: 0)) // the beginning of the trough
+
+        // first curve down
+        path.addCurve(to: CGPoint(x: centerWidth, y: height),
+                      controlPoint1: CGPoint(x: (centerWidth - 30), y: 0), controlPoint2: CGPoint(x: centerWidth - 35, y: height))
+        // second curve up
+        path.addCurve(to: CGPoint(x: (centerWidth + height * 2), y: 0),
+                      controlPoint1: CGPoint(x: centerWidth + 35, y: height), controlPoint2: CGPoint(x: (centerWidth + 30), y: 0))
+
+        // complete the rect
+        path.addLine(to: CGPoint(x: self.frame.width, y: 0))
+        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        path.addLine(to: CGPoint(x: 0, y: self.frame.height))
+        path.close()
+
         return path.cgPath
     }
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        self.isTranslucent = true
-        var tabFrame = self.frame
-        let window = UIApplication.shared.connectedScenes.compactMap { ($0 as? UIWindowScene)?.keyWindow }.last
-        let bottomPadding = window?.safeAreaInsets.bottom
-        tabFrame.size.height = 65 + (bottomPadding ?? 0)
-        tabFrame.origin.y = self.frame.origin.y + self.frame.height - 65 - (bottomPadding ?? 0)
-        self.layer.cornerRadius = 18
-        self.frame = tabFrame
-        self.items?.forEach({ $0.titlePositionAdjustment = UIOffset(horizontal: 0.0, vertical: -5.0) })
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let buttonRadius: CGFloat = 35
+        return abs(self.center.x - point.x) > buttonRadius || abs(point.y) > buttonRadius
     }
+
+    func createPathCircle() -> CGPath {
+
+        let radius: CGFloat = 37.0
+        let path = UIBezierPath()
+        let centerWidth = self.frame.width / 2
+
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: (centerWidth - radius * 2), y: 0))
+        path.addArc(withCenter: CGPoint(x: centerWidth, y: 0), radius: radius, startAngle: CGFloat(180).degreesToRadians, endAngle: CGFloat(0).degreesToRadians, clockwise: false)
+        path.addLine(to: CGPoint(x: self.frame.width, y: 0))
+        path.addLine(to: CGPoint(x: self.frame.width, y: self.frame.height))
+        path.addLine(to: CGPoint(x: 0, y: self.frame.height))
+        path.close()
+        return path.cgPath
+    }
+}
+
+extension CGFloat {
+    var degreesToRadians: CGFloat { return self * .pi / 180 }
+    var radiansToDegrees: CGFloat { return self * 180 / .pi }
 }
