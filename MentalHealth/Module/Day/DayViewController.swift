c38d6976d1ed043086a9e913ccd8144343a6c0dd
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-04-04 21:49:41 -0500

    feat: create initial day feature

---
+++ MentalHealth/Module/Day/DayViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DayViewController.swift
+//  MentalHealth
+//
+//  Created by JungpyoHong on 4/4/24.
+//
+
+import UIKit
+
+class DayViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
