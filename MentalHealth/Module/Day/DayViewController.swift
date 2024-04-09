acc67d1be7b70ba76659f4ab2cc9c9bc94ee52f9
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-04-08 23:38:33 -0500

    feat: add feeling option view

--- MentalHealth/Module/Day/DayViewController.swift
+++ MentalHealth/Module/Day/DayViewController.swift
@@ -17,9 +17,26 @@ class DayViewController: UIViewController {
     @IBOutlet weak var saturdayView: UIView!
     
     @IBOutlet weak var welcomeView: UIView!
+    @IBOutlet weak var nameLabel: UILabel! {
+        didSet {
+            self.nameLabel.text = "Hi Nickname!"
+        }
+    }
+    
+    @IBOutlet weak var feelingOptionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         makeCircleShape(welcomeView)
         applyBoader([sundayView, mondayView, tuesdayView, wednesdayView, thursdayView, fridayView, saturdayView], with: UIColor.tabBarBorder)
+        applyStyle(feelingOptionView)
+    }
+    
+    func applyStyle(_ view: UIView) {
+        view.layer.cornerRadius = view.layer.frame.height/2
+        view.layer.backgroundColor = UIColor.white.cgColor
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowOffset = CGSize(width: 0, height: 2)
     }
 }
