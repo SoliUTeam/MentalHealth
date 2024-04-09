07e9aaff390ac7b607a32ed72230bf96f374ef04
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-04-08 22:30:44 -0500

    feat: update calendar

--- MentalHealth/Module/Day/DayViewController.swift
+++ MentalHealth/Module/Day/DayViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class DayViewController: UIViewController {
+    @IBOutlet weak var sundayView: UIView!
+    @IBOutlet weak var mondayView: UIView!
+    @IBOutlet weak var tuesdayView: UIView!
+    @IBOutlet weak var wednesdayView: UIView!
+    @IBOutlet weak var thursdayView: UIView!
+    @IBOutlet weak var fridayView: UIView!
+    @IBOutlet weak var saturdayView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        applyBoader([sundayView, mondayView, tuesdayView, wednesdayView, thursdayView, fridayView, saturdayView], with: UIColor.tabBarBorder)
     }
 }
