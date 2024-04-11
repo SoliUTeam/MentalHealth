7a078b027526fedb4eef827b95652ca7be9b61f3
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-04-10 23:13:11 -0500

    edit: main page

---
+++ MentalHealth/Module/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  MentalHealth
+//
+//  Created by JungpyoHong on 4/10/24.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
