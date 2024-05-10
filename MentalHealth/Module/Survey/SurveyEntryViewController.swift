8e4c0019db0f85d501dd240529959dd035d7be73
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2024-05-09 22:26:52 -0500

    chore: fix tab bar item

--- MentalHealth/Module/Survey/SurveyEntryViewController.swift
+++ MentalHealth/Module/Survey/SurveyEntryViewController.swift
@@ -9,18 +9,21 @@ import UIKit
 
 class SurveyEntryViewController: UIViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCustomBackNavigationButton()
     }
-    
-    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
     @IBAction func openSurveyListViewController() {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         
         if let surveyListVC = storyboard.instantiateViewController(identifier: "SurveyListViewController") as? SurveyListViewController {
             navigationController?.pushViewController(surveyListVC, animated: true)
-            
         } else {
             print("Can't find storyboard")
         }
